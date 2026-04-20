3bf0111d5070a5e0a81dc8757f9ae6ef667faea8
ArmisC <armiscanollii@gmail.com>
2026-04-20 14:58:43 +0100

    Redirect root to home

--- supplements/urls.py
+++ supplements/urls.py
@@ -18,9 +18,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import RedirectView
 
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='/products/home/', permanent=False)),
     path('admin/', admin.site.urls),
     path('products/', include('product.urls')),
     path('users/', include('users.urls')),
